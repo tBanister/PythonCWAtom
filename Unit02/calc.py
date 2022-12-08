80f16b4a779148395f8277ad256be101cbefaae2
tBanister <tombradley_banister@hotmail.co.uk>
2022-12-08 12:15:56 +0000

    081222AM

---
+++ Unit02/calc.py
@@ -0,0 +1,17 @@
+nums = []
+counter = 0
+newnum = 0
+amount = int(input("How many inputs?"))
+
+while counter < amount:
+    newnum = int(input("new number"))
+    print(newnum)
+    nums.append(newnum)
+    counter += 1
+
+
+
+    
+
+
+
