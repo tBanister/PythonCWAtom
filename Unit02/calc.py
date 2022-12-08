ff0e925f59b0cc565f3a67f2506ca61268bcc664
tBanister <tombradley_banister@hotmail.co.uk>
2022-12-08 14:37:57 +0000

    Calc

--- Unit02/calc.py
+++ Unit02/calc.py
@@ -1,13 +1,29 @@
 nums = []
 counter = 0
 newnum = 0
+total = 0
+sum = 1
 amount = int(input("How many inputs?"))
 
 while counter < amount:
     newnum = int(input("new number"))
-    print(newnum)
     nums.append(newnum)
+    total += newnum
+    sum *= newnum
     counter += 1
+
+
+done = False
+while done == False:
+    AorM = input("'a' for Addition or 'm' for Multiply")
+    if AorM.lower() == "a":
+        print("Addition =",total)
+        done = True
+    elif AorM.lower() == "m":
+        print("Multiply =", sum)
+        done = True
+    else:
+        print("'a' or 'm' only")
 
 
 
